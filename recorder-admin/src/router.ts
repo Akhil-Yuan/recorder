2541526bde70b3a7dc3c845b4c207a48e8644eac
aki-yuan <yxqi09@126.com>
2021-08-25 16:12:52 +0800

    feat: create views of 404 and dashboard

--- recorder-admin/src/router.ts
+++ recorder-admin/src/router.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Layout from '@/layout/index.vue'
 
 Vue.use(Router)
 
@@ -17,6 +18,26 @@ export default new Router({
       path: '/login',
       component: () => import('@/views/login/index.vue'),
       meta: { hidden: true }
+    },
+    {
+      path: '/404',
+      component: () => import('@/views/404.vue'),
+      meta: { hidden: true }
+    },
+    {
+      path: '/',
+      component: Layout,
+      redirect: '/dashboard',
+      children: [
+        {
+          path: 'dashboard',
+          component: () => import('@/views/dashboard/index.vue'),
+          meta: {
+            title: 'Dashboard',
+            icon: 'dashboard'
+          }
+        }
+      ]
     }
   ]
 })
