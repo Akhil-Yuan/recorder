5b5f4afc227c7f5aa51b6b002804a283698d5a87
Crazy_pea <yarh19898510903@163.com>
2022-04-02 23:15:25 +0800

    课件

---
+++ recorder-pc/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="con">
+    <Courseware />
+    <Homework />
+  </div>
+</template>
+
+<script>
+import Courseware from './Pages/Courseware'
+import Homework from './Pages/Homework'
+export default {
+  name: 'App',
+  components: {
+    Courseware,
+    Homework
+  }
+}
+</script>
+
+<style>
+
+</style>
