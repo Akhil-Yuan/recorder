cbdf567f39f400c3ec902f84df7342962afd8ee2
Crazy_pea <yarh19898510903@163.com>
2022-05-13 19:33:33 +0800

    储存token

--- recorder-pc/src/utils/request.js
+++ recorder-pc/src/utils/request.js
@@ -45,11 +45,11 @@ const errorHandler = (error) => {
 // request interceptor
 request.interceptors.request.use(config => {
   nProgress.start()
-  const token = storage.get('Access-Token')
+  const token = storage.get('token')
   // 如果 token 存在
   // 让每个请求携带自定义 token 请根据实际情况自行修改
   if (token) {
-    config.headers['Access-Token'] = token
+    config.headers['token'] = token
   }
   return config
 }, errorHandler)
